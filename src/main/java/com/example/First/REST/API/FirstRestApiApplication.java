88af4787c4aa0e8219bdf8fcd333513dfa5f0d42
Vedant <vedantdumbre0706@gmail.com>
2026-03-26 21:40:34 +0530

    Configured SpringBoot

---
+++ src/main/java/com/example/First/REST/API/FirstRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.First.REST.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/First/REST/API/FirstRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.First.REST.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
