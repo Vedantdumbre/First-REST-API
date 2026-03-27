fc8db0c4fd8de1a75eb38bc6805186241a3c6fb4
Vedant <vedantdumbre0706@gmail.com>
2026-03-28 01:50:10 +0530

    Making dynamic URLs using @Pathvariable

--- src/main/java/com/example/First/REST/API/HelloController.java
+++ src/main/java/com/example/First/REST/API/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.First.REST.API;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloParam(@PathVariable String name){ // we can make dynamic URLs using @PathVariable
+        return new HelloResponse("Hello "+ name);
+    }
 
     @GetMapping("/hello")
     public HelloResponse hello(){
